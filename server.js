07996d0415318700426bbfbc011903d4ac589ab7
kimnana45 <kimle.lek@gmail.com>
2020-08-01 12:15:06 -0400

    deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/heroku_2kq72k4g", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true });
 
 // routesnode server
 app.use(require("./routes/api.js"));
